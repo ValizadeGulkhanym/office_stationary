cfd670dee4767f950410ce748a10120273773aaa
Valizade Gulkhanym <gulyaaliyeva1990@gmail.com>
2024-01-31 22:36:41 +0400

    *

--- en/js/main.js
+++ en/js/main.js
@@ -1340,3 +1340,47 @@ function OwlLoaded(el) {
     if ($(el).parents('.tab-pane').length && !$(el).parents('.tab-pane').hasClass('active'))
         el.width('100%');
 };
+
+//carousel
+
+ap_list_functions.push(function() {
+    $('#carousel-2612739272').imagesLoaded(function() {
+        $('#carousel-2612739272').owlCarousel({
+            items: 1,
+            itemsDesktop: [1200, 1],
+            itemsDesktopSmall: [992, 1],
+            itemsTablet: [768, 1],
+            itemsMobile: [576, 1],
+            itemsCustom: false,
+            singleItem: false, // true : show only 1 item
+            itemsScaleUp: false,
+            slideSpeed: 200, //  change speed when drag and drop a item
+            paginationSpeed: 800, // change speed when go next page
+            autoPlay: false, // time to show each item
+            stopOnHover: false,
+            navigation: false,
+            navigationText: ["&lsaquo;", "&rsaquo;"],
+            scrollPerPage: false,
+            pagination: true, // show bullist
+            paginationNumbers: false, // show number
+            responsive: true,
+            lazyLoad: false,
+            lazyFollow: false, // true : go to page 7th and load all images page 1...7. false : go to page 7th and load only images of page 7th
+            lazyEffect: "fade",
+            autoHeight: false,
+            mouseDrag: true,
+            touchDrag: true,
+            addClassActive: true,
+            direction: false,
+            afterInit: OwlLoaded,
+            afterAction: SetOwlCarouselFirstLast,
+        });
+    });
+});
+
+function OwlLoaded(el) {
+    el.removeClass('owl-loading').addClass('owl-loaded').parents('.owl-row').addClass('hide-loading');
+};
+
+
+
